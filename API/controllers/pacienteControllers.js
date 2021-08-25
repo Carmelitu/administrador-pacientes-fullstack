176fe9025ae97bf2075ff774d7ac8169cfe5dba5
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-24 22:39:42 -0300

    backend finalizado, modificar y eliminar registros agregado

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -11,9 +11,55 @@ exports.nuevoCliente = async (req, res, next) => {
         console.log(paciente);
         
     } catch (error) {
-        console.error(error);
+        console.log(error);
         next();
     }
 
 
+}
+
+// Obtiene todos los pacientes
+exports.obtenerPacientes = async (req, res, next) => {
+    try {
+        const pacientes = await Paciente.find({});
+        res.json(pacientes);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+// Obtiene paciente por Id
+exports.obtenerPaciente = async (req, res, next) => {
+    try {
+        const paciente = await Paciente.findById(req.params.id);
+        res.json(paciente);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+// Actualiza registro por su ID
+exports.actualizarPaciente = async (req, res, next) => {
+    try {
+        const paciente = await Paciente.findOneAndUpdate({_id : req.params.id}, req.body, {
+            new: true
+        });
+        res.json(paciente);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+// Elimina paciente por ID
+exports.eliminarPaciente = async (req, res, next) => {
+    try {
+        await Paciente.findOneAndDelete({_id : req.params.id});
+        res.json({mensaje: 'El paciente fue eliminado'});
+    } catch (error) {
+        console.log(error);
+        next();
+    }
 }
--- API/routes/index.js
+++ API/routes/index.js
@@ -7,7 +7,17 @@ module.exports = function () {
     // Agrega nuevos pacientes via POST
     router.post('/pacientes', pacienteController.nuevoCliente);
 
+    // Obtiene registros en DB
+    router.get('/pacientes', pacienteController.obtenerPacientes);
 
+    // Obtiene paciente con ID
+    router.get('/pacientes/:id', pacienteController.obtenerPaciente);
+
+    // Actualizar paciente con ID
+    router.put('/pacientes/:id', pacienteController.actualizarPaciente);
+
+    // Elimina paciente con ID
+    router.delete('/pacientes/:id', pacienteController.eliminarPaciente);
 
     return router;
 }
