c4ff377dcba87fb49587fe1a0dcac335bb42a456
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-26 09:30:08 -0300

    configuraciones de electron para generar app de escritorio

--- Desktop/index.js
+++ Desktop/index.js
@@ -4,6 +4,12 @@ let appWindow
 
 function crearVentana(){
     appWindow = new BrowserWindow({
+        width: 1200,
+        height: 860,
+        minWidth: 800,
+        minHeight: 600,
+        center: true,
+        show: false
 
     });
 
